ed214cae4b4be77fff815b3da9afe49275d318ba
Okiki <okikio.dev@gmail.com>
2023-04-07 06:26:47 +0000

    chore: remove index.mjs

--- index.mjs
+++
@@ -1,9 +0,0 @@
-
-// import { adapter, type NodeAdapterOptions } from "astro-auto-adapter";
-
-/** @type {import('./src/index.js').NodeAdapterOptions} */
-const options = {
-  // Configuration options go here
-};
-
-const astroAdapter = adapter("node", { node: options });
